333be5a06e3ff23e5d0a754bebd5d16d2b788cf8
Subhi Katabi <mkatabi96@gmail.com>
2020-05-26 23:11:12 -0700

    initial commit

---
+++ src/test/java/com/skatabi/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skatabi.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
